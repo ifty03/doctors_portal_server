cc4e34fe2305ab118579334119795b28aed266bd
ifty03 <ashikulislamb84@gmail.com>
2022-05-12 10:53:39 +0600

    first server connetc

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+/* middleware */
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3zjl8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("doctors_db").collection("tritment");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const service = await serviceCollection.find(query).toArray();
+      res.send(service);
+    });
+  } finally {
+    // await client.close();
+  }
+};
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
